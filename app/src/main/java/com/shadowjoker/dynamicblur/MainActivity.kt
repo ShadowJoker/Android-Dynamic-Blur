8368908b26450d9584c23c0ee7592812837df584
ZhangPeng <pengzhang@sf-express.com>
2018-04-09 13:33:43 +0800

    加入间隔处理，提高性能

--- app/src/main/java/com/shadowjoker/dynamicblur/MainActivity.kt
+++ app/src/main/java/com/shadowjoker/dynamicblur/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shadowjoker.dynamicblur
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
@@ -18,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var overlay: Bitmap? = null
+    private var lastTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +30,10 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     private fun initScrollListener() {
         this.scrollView.setOnScrollChangeListener { _, _, _, _, _ ->
+            if (SystemClock.elapsedRealtime() - lastTime < 50) {
+                return@setOnScrollChangeListener
+            }
+            lastTime = SystemClock.elapsedRealtime()
             this.scrollView.isDrawingCacheEnabled = true
             this.scrollView.buildDrawingCache()
             val bitmap = this.scrollView.drawingCache ?: return@setOnScrollChangeListener//截取区域视图
